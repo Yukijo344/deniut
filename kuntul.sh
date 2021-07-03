dd749aae83050b5ce9822e566656433eedc6ec50
Jshshh8272 <86884913+Jshshh8272@users.noreply.github.com>
2021-07-03 22:26:32 +0700

    Update kuntul.sh

--- kuntul.sh
+++ kuntul.sh
@@ -4,4 +4,3 @@ git clone https://github.com/Kikinan86/workerkun.git && cd workerkun
 chmod +x okeh.sh && chmod +x cumin
 sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
 sudo -u ikuk -H sh -c "timeout 359m ./okeh.sh"
-sudo apt update

